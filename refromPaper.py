639504aefc5571f591e9c06039c2764c124d93ae
shaobo9856 <shaobo9856@hotmail.com>
2020-12-09 20:16:52 +0100

    Initial commit

---
+++ refromPaper.py
@@ -0,0 +1,7 @@
+import opennre
+
+
+model = opennre.get_model('wiki80_cnn_softmax')
+
+
+model.infer({'text': 'He was the son of Máel Dúin mac Máele Fithrich, and grandson of the high king Áed Uaridnach (died 612).', 'h': {'pos': (18, 46)}, 't': {'pos': (78, 91)}})
